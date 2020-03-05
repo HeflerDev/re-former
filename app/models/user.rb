ef32025e2358b2641c46161880ccba9f68b8ca70
Kasuhira <hheflerdevelopment@gmail.com>
2020-03-05 19:05:33 -0300

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 4..16 }
+  validates :email, presence:true, uniqueness:true
+  validates :password, presence:true, length: { in: 4..16 }
 end
