ea758815aff70ecd47ac8ccc7a1e98977e674b75
Gwen <itsgwenhey@gmail.com>
2020-03-06 14:20:54 -0600

    rubocop fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  
   def new
     @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -13,7 +12,6 @@ class UsersController < ApplicationController
     else
       render :new
     end
-  
   end
 
   def edit
@@ -30,8 +28,8 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:username,:email,:password)
-  end
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
